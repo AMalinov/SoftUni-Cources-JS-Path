65abf2cfb4182be069551e986514855656729ffb
Asen Malinov <m4linov@gmail.com>
2022-06-03 21:16:50 +0300

    Fixed dynamic options in attach page

--- JS-Back-End-May-2022/workshop-cubicles/src/controllers/cubeController.js
+++ JS-Back-End-May-2022/workshop-cubicles/src/controllers/cubeController.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const cubeService = require('../services/cubeService');
+const accessoryService = require('../services/accessoryService');
 
 router.get('/create', (req, res) => {
     res.render('create');
@@ -32,7 +33,14 @@ router.get('/details/:id', async (req, res) => {
 
 router.get('/:cubeId/attach-accessory', async (req, res) => {
     const cube = await cubeService.getOne(req.params.cubeId).lean();
-    res.render('accessory/attach', { cube });
+    const accessories = await accessoryService.getAll().lean();
+    res.render('accessory/attach', { cube, accessories });
+});
+
+router.post('/:cubeId/attach-accessory', (req, res) => {
+    const accessoryId = req.body.accessory;
+
+    res.redirect(`/cube/details/${req.params.cubeId}`);
 });
 
 module.exports = router;
