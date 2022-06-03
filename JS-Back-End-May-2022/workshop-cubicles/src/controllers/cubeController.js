2552bee765cda2ffdce7cd4a6ffdcb7c2c4583d6
Asen Malinov <m4linov@gmail.com>
2022-06-03 16:38:53 +0300

    Added first workshop - Cubicles

---
+++ JS-Back-End-May-2022/workshop-cubicles/src/controllers/cubeController.js
@@ -0,0 +1,32 @@
+const router = require('express').Router();
+const cubeService = require('../services/cubeService');
+
+router.get('/create', (req, res) => {
+    res.render('create');
+});
+
+router.post('/create', async (req, res) => {
+    const cube = req.body;
+
+    if (cube.name.length < 2) {
+        return res.status(400).send('Invalid request');
+    }
+
+    try {
+        await cubeService.save(cube);
+
+        res.redirect('/');
+    } catch (err) {
+        res.status(400).send(err);
+    }
+
+
+});
+
+
+router.get('/details/:id',(req, res) => {
+    const cube = cubeService.getOne(req.params.id);
+
+    res.render('details', { cube });
+});
+module.exports = router;
