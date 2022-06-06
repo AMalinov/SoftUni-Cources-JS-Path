5815f60d195856df3d931721d44ebd8246389260
Asen Malinov <m4linov@gmail.com>
2022-06-06 12:35:04 +0300

    Disabled attach button if no accessory exists

--- JS-Back-End-May-2022/workshop-cubicles/src/services/cubeService.js
+++ JS-Back-End-May-2022/workshop-cubicles/src/services/cubeService.js
@@ -18,7 +18,9 @@ exports.getAll = async (search = '', fromInput, toInput) => {
     // return [];
 };
 
-exports.getOne = (cubeId) => Cube.findById(cubeId).populate('accessories');
+exports.getOne = (cubeId) => Cube.findById(cubeId);
+
+exports.getOneDetailed = (cubeId) => Cube.findById(cubeId).populate('accessories');
 
 exports.create = (cube) => Cube.create(cube);
 
