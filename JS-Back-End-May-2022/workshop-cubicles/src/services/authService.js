2b7fb16402915780dd8192b6a6ae09ac4bdb28b0
Asen Malinov <m4linov@gmail.com>
2022-06-11 16:47:33 +0300

    Create jwt token on login

--- JS-Back-End-May-2022/workshop-cubicles/src/services/authService.js
+++ JS-Back-End-May-2022/workshop-cubicles/src/services/authService.js
@@ -1,7 +1,9 @@
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 const User = require('../models/User');
 
 const saltRounds = 10;
+const secret = 'asdfdsgffdhfddsfasdasdasdasdldas;asdasdfgsdsd;la';
 
 exports.register = async ({ username, password, repeatPassword }) => {
     // TODO: return form validation message
@@ -29,9 +31,20 @@ exports.login = async ({ username, password }) => {
 
     const isValid = await bcrypt.compare(password, user.password);
 
-    if (isValid) {
-        return user;
-    } else {
+    if (!isValid) {
         return;
     }
+
+    let result = new Promise((resolve, reject) => {
+        jwt.sign({ _id: user._id, username: user.username }, secret, { expiresIn: '2d' }, (err, token) => {
+        
+            if (err) {
+                return reject(err);
+            }
+    
+            resolve(token);
+        });
+    });
+
+    return result;
 };
