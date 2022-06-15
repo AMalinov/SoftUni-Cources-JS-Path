06c532284fde20574965dd968a02f97c11e6892c
Asen Malinov <m4linov@gmail.com>
2022-06-15 22:36:57 +0300

    Added secret, salt and logout logic

--- JS-Back-End-May-2022/exam-prep-art-gallery/controllers/authController.js
+++ JS-Back-End-May-2022/exam-prep-art-gallery/controllers/authController.js
@@ -1,9 +1,21 @@
 const router = require('express').Router();
 
+const { COOKIE_SESSION_NAME } = require('../constants');
 const authService = require('../services/authService');
 
 router.get('/login', (req, res) => {
     res.render('auth/login');
+});
+
+router.post('/login', async (req, res) => {
+    const { username, password } = req.body;
+
+    const user = await authService.login(username, password);
+    const token = await authService.createToken(user);
+
+    res.cookie(COOKIE_SESSION_NAME, token);
+
+    res.redirect('/');
 });
 
 router.get('/register', (req, res) => {
@@ -19,13 +31,21 @@ router.post('/register', async (req, res) => {
 
     // Create user
     try {
-        await authService.create({password, ...userData});
+        const createdUser = await authService.create({ password, ...userData });
+        const token = await authService.createToken(createdUser);
+
+        res.cookie(COOKIE_SESSION_NAME, token);
         res.redirect('/');
     } catch (error) {
         // Add mongoose error mapper
         return res.render('auth/register', { error: 'db error' });
     }
 
+});
+
+router.get('/logout', (req, res) => {
+    res.clearCookie(COOKIE_SESSION_NAME);
+    res.redirect('/');
 });
 
 module.exports = router;
--- JS-Back-End-May-2022/exam-prep-art-gallery/services/authService.js
+++ JS-Back-End-May-2022/exam-prep-art-gallery/services/authService.js
@@ -1,3 +1,41 @@
 const User = require('../models/User');
 
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+
+const { SECRET } = require('../config/enviorments');
+
 exports.create = (userdata) => User.create(userdata);
+exports.login = async (username, password) => {
+    const user = await User.findOne({ username });
+
+    if (!user) {
+        throw {
+            message: 'Cannot find username or password'
+        };
+    }
+
+    const isValid = bcrypt.compare(password, user.password);
+
+    if (!isValid) {
+        throw {
+            message: 'Cannot find username or password'
+        };
+    }
+
+    return user;
+};
+
+exports.createToken = (user) => {
+    const payload = { _id: user._id, username: user.username, address: user.address };
+
+    const options = { expiresIn: '2d' };
+    return new Promise((resolve, reject) => {
+        jwt.sign(payload, SECRET, options, (err, decodedToken) => {
+            if (err) {
+                return reject(err);
+            }
+            resolve(decodedToken);
+        });
+    });
+};
