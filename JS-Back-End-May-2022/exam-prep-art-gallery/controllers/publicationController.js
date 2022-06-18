d8761a1c94e0725794a0e52191085893607bb7a3
Asen Malinov <m4linov@gmail.com>
2022-06-18 23:29:17 +0300

    Added create and details page

--- JS-Back-End-May-2022/exam-prep-art-gallery/controllers/publicationController.js
+++ JS-Back-End-May-2022/exam-prep-art-gallery/controllers/publicationController.js
@@ -1,7 +1,30 @@
 const router = require('express').Router();
 
-router.get('/create', (req,res) => {
+const { isAuth } = require('../middlewares/authMiddleware');
+const publicationService = require('../services/publicationService');
+
+router.get('/', async (req, res) => {
+    const publications = await publicationService.getAll().lean();
+    res.render('publication', { publications });
+});
+
+router.get('/:publicationId/details', async (req, res) => {
+    const publication = await publicationService.getOneDetailed(req.params.publicationId).lean();
+
+    res.render('publication/details', { ...publication });
+});
+
+router.get('/create', isAuth, (req, res) => {
     res.render('publication/create');
+});
+
+router.post('/create', isAuth, async (req, res) => {
+    const publicationData = { ...req.body, author: req.user._id };
+
+    await publicationService.create(publicationData);
+
+    res.redirect('/publications');
+
 });
 
 module.exports = router;
---
+++ JS-Back-End-May-2022/exam-prep-art-gallery/services/publicationService.js
@@ -0,0 +1,8 @@
+const Publication = require('../models/Publication');
+
+exports.getAll = () => Publication.find();
+
+exports.getOne = (publicationId) => Publication.findById(publicationId);
+exports.getOneDetailed = (publicationId) => Publication.findById(publicationId).populate('author');
+exports.create = (publicationData) => Publication.create(publicationData);
+
