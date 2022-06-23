ec7330eae330379fdb4e1edfbc28e9dba3b96cda
Asen Malinov <m4linov@gmail.com>
2022-06-23 14:16:49 +0300

    Create user service, implement profile page and share publications

--- JS-Back-End-May-2022/exam-prep-art-gallery/controllers/publicationController.js
+++ JS-Back-End-May-2022/exam-prep-art-gallery/controllers/publicationController.js
@@ -3,6 +3,7 @@ const router = require('express').Router();
 const { isAuth } = require('../middlewares/authMiddleware');
 const { isPublicationAuthor, preloadPublication } = require('../middlewares/publicationMiddlewares');
 const publicationService = require('../services/publicationService');
+const userService = require('../services/userService');
 const { getErrorMessage } = require('../utils/errorHelpers');
 
 router.get('/', async (req, res) => {
@@ -15,7 +16,9 @@ router.get('/:publicationId/details', async (req, res) => {
 
     const isAuthor = publication.author._id == req.user?._id;
 
-    res.render('publication/details', { ...publication, isAuthor });
+    const isShared = publication.usersShared.some(x => x._id == req.user._id);
+
+    res.render('publication/details', { ...publication, isAuthor, isShared });
 });
 
 
@@ -26,12 +29,18 @@ router.get('/:publicationId/edit', preloadPublication, isPublicationAuthor, isAu
 
 router.post('/:publicationId/edit', isAuth, preloadPublication, isPublicationAuthor, async (req, res) => {
     try {
-        await publicationService.updateOne(req.params.publicationId, req.body);
+        await publicationService.update(req.params.publicationId, req.body);
 
         res.redirect(`/publications/${req.params.publicationId}/details`);
     } catch (error) {
         res.render('publication/edit', { ...req.body, error: getErrorMessage(error) });
     }
+});
+
+router.get('/:publicationId/delete', isAuth, preloadPublication, isPublicationAuthor, async (req, res) => {
+    await publicationService.delete(req.params.publicationId);
+    res.redirect('/publications');
+    // res.render('publication/delete', {...req.publication});
 });
 
 router.get('/create', isAuth, (req, res) => {
@@ -42,13 +51,26 @@ router.post('/create', isAuth, async (req, res) => {
     const publicationData = { ...req.body, author: req.user._id };
 
     try {
-        await publicationService.create(publicationData);
-
+        const publication = await publicationService.create(publicationData);
+        await userService.addPublication(req.user._id, publication._id);
         res.redirect('/publications');
 
     } catch (error) {
         res.render('publication/create', { ...req.body, error: getErrorMessage(error) });
     }
+});
+
+router.get('/:publicationId/share', isAuth, async (req, res) => {
+    const publication = await publicationService.getOne(req.params.publicationId);
+    const user = await userService.getOne(req.user._id);
+
+    publication.usersShared.push(req.user._id);
+    user.shares.push(publication);
+
+    await publication.save();
+    await user.save();
+
+    res.redirect('/');
 });
 
 module.exports = router;
--- JS-Back-End-May-2022/exam-prep-art-gallery/services/publicationService.js
+++ JS-Back-End-May-2022/exam-prep-art-gallery/services/publicationService.js
@@ -1,8 +1,10 @@
 const Publication = require('../models/Publication');
+const User = require('../models/User');
 
 exports.getAll = () => Publication.find();
 
 exports.getOne = (publicationId) => Publication.findById(publicationId);
 exports.getOneDetailed = (publicationId) => Publication.findById(publicationId).populate('author');
+exports.update = (publicationId, publicationData) => Publication.updateOne({ _id: publicationId }, { $set: publicationData }, { runValidators: true });
+exports.delete = (publicationId) => Publication.deleteOne({ _id: publicationId });
 exports.create = (publicationData) => Publication.create(publicationData);
-exports.update = (publicationId, publicationData) => Publication.updateOne({_id: publicationId}, {$set: publicationData}, {runValidators: true});
