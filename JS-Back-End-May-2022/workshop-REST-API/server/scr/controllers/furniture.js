d1a854e5cc543b1ff1b007c6e9dbcabde249c834
Asen Malinov <m4linov@gmail.com>
2022-07-02 15:23:23 +0300

    Implement update and delete items

--- JS-Back-End-May-2022/workshop-REST-API/server/scr/controllers/furniture.js
+++ JS-Back-End-May-2022/workshop-REST-API/server/scr/controllers/furniture.js
@@ -24,6 +24,57 @@ router.post('/', async (req, res) => {
         console.error(err);
         res.status(400).json({ message: 'Request error' });
     }
-})
+});
+
+router.get('/:id', async (req, res) => {
+    const id = req.params.id;
+
+    const item = await api.getById(id);
+
+    if (item) {
+        res.json(item);
+    } else {
+        res.status(404).json({ message: `Item ${id} not found!` });
+
+    }
+});
+
+router.put('/:id', async (req, res) => {
+    const id = req.params.id;
+
+    const item = {
+        make: req.body.make,
+        model: req.body.model,
+        year: req.body.year,
+        description: req.body.description,
+        price: req.body.price,
+        img: req.body.img,
+        material: req.body.material
+    };
+
+    try {
+        const result = await api.updateById(id, item);
+        res.json(result);
+    } catch (err) {
+        if (err._notFound) {
+            res.status(404).json({ message: `Item ${id} not found!` });
+        } else {
+            console.error(err);
+            res.status(400).json({ message: 'Request error' });
+        }
+    }
+});
+
+router.delete('/:id', async (req, res) => {
+    const id = req.params.id;
+
+    try {
+        const result = await api.deleteById(id);
+        res.json(result);
+    } catch (err) {
+        console.error(err);
+        res.status(404).json({ message: `Item ${id} not found!` });
+    }
+});
 
 module.exports = router;
--- JS-Back-End-May-2022/workshop-REST-API/server/scr/services/futniture.js
+++ JS-Back-End-May-2022/workshop-REST-API/server/scr/services/futniture.js
@@ -21,7 +21,40 @@ async function create(item) {
     return result;
 }
 
+async function getById(id) {
+    return await Item.findById(id);
+}
+
+async function updateById(id, item) {
+    const existing = await Item.findById(id);
+
+    if (existing) {
+        existing.make = item.make;
+        existing.model = item.model;
+        existing.year = item.year;
+        existing.description = item.description;
+        existing.price = item.price;
+        existing.img = item.img;
+        existing.material = item.material;
+
+        await existing.save();
+
+        return existing;
+    } else {
+        const error = new Error('Not Found'); 
+        error._notFound = true;
+        throw error;
+    }
+}
+
+async function deleteById(id) {
+    await Item.findByIdAndDelete(id);
+}
+
 module.exports = {
     getAll,
-    create
+    create,
+    getById,
+    updateById,
+    deleteById
 };
